4c34f92bb5c6f3e67aa4091eaaecc6736a9c4aa7
Irma Džemidžić <irma.karasoftic@gmail.com>
2018-12-03 14:53:04 +0100

    Add files via upload

---
+++ src/app/shared/card.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+  @Input() item: any;
+  @Input() flipped: boolean;
+  @Output() onEdit = new EventEmitter<any>();
+  @Output() onDelete = new EventEmitter<any>();
+  @Output() onVersionCreate = new EventEmitter<any>();
+  @Output() onVersionEdit = new EventEmitter<any>();
+  @Output() onVersionDelete = new EventEmitter<any>();
+
+  ngOnInit() {
+    console.log('iz card', this.item);
+  }
+  edit(item: any) {
+    console.log(item);
+    this.onEdit.emit(item);
+  }
+  delete(item: any) {
+    console.log(item);
+    this.onDelete.emit(item);
+  }
+  createVersion(app) {
+    console.log();
+    this.onVersionCreate.emit(app);
+  }
+  editVersion(item: any) {
+    console.log(item);
+    this.onVersionEdit.emit(item);
+  }
+  deleteVersion(item: any) {
+    console.log(item);
+    this.onVersionDelete.emit(item);
+  }
+  flipIt() {
+    this.flipped = !this.flipped;
+  }
+}
